495f7cc8503cb61b5117b928c69e0798cc7a7107
AmanKumarsingh23 <amansingh23@iitk.ac.in>
2026-01-08 19:10:32 +0530

    Feat: Implemented Commit logic and Branch Head tracking

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <filesystem> // Used to create folders
 #include <fstream>    // Used to create/read files
+#include <chrono>
+#include <ctime>
+
 
 using namespace std;
 namespace fs = filesystem; // A shortcut so we don't have to type 'filesystem' every time
@@ -9,6 +12,10 @@ namespace fs = filesystem; // A shortcut so we don't have to type 'filesystem' e
 const string MINI_GIT_DIR = ".minigit";
 const string OBJECTS_DIR = MINI_GIT_DIR + "/objects";
 const string INDEX_FILE = MINI_GIT_DIR + "/index.txt"; 
+const string HEAD_FILE = MINI_GIT_DIR + "/HEAD";
+const string BRANCHES_FILE = MINI_GIT_DIR + "/branches.txt";
+const string COMMITS_DIR = MINI_GIT_DIR + "/commits";
+
 
 
 // This turns file content into a unique string ID
@@ -48,17 +55,110 @@ void add(const string& filename) {
 }
 
 void init() {
-    // Check if the folder already exists so we don't overwrite it
     if (fs::exists(MINI_GIT_DIR)) {
         cout << "MiniGit is already initialized here!" << endl;
         return;
     }
 
-    // Create the folders
     fs::create_directory(MINI_GIT_DIR);
     fs::create_directory(OBJECTS_DIR);
+    fs::create_directory(COMMITS_DIR);
 
-    cout << "Success! Created .minigit and .minigit/objects" << endl;
+    // Create initial branch
+    ofstream branches(BRANCHES_FILE);
+    branches << "main:null\n";  // no commits yet
+    branches.close();
+
+    // Set HEAD to main
+    ofstream head(HEAD_FILE);
+    head << "main";
+    head.close();
+
+    // Empty index
+    ofstream index(INDEX_FILE);
+
+    cout << "Success! Created .minigit structure" << endl;
+}
+
+
+// Gets the name of the branch we are currently on (e.g., "main")
+string getCurrentBranch() {
+    ifstream headIn(HEAD_FILE);
+    string branch;
+    getline(headIn, branch);
+    return branch;
+}
+
+// Finds the most recent Commit Hash for a specific branch
+string getBranchHead(const string& branchName) {
+    ifstream branchesIn(BRANCHES_FILE);
+    string line;
+    while (getline(branchesIn, line)) {
+        size_t sep = line.find(":");
+        if (line.substr(0, sep) == branchName) {
+            return line.substr(sep + 1);
+        }
+    }
+    return "null"; // If no commits yet
+}
+
+// Updates the branch to point to the NEWEST commit (the new head)
+void updateBranchHead(const string& branch, const string& newHash) {
+    ifstream in(BRANCHES_FILE);
+    stringstream updated;
+    string line;
+    while (getline(in, line)) {
+        size_t sep = line.find(":");
+        if (line.substr(0, sep) == branch) {
+            updated << branch << ":" << newHash << "\n";
+        } else {
+            updated << line << "\n";
+        }
+    }
+    ofstream out(BRANCHES_FILE);
+    out << updated.str();
+}
+
+
+void commit(const string& message) {
+    // 1. Check if there is anything to commit
+    ifstream indexIn(INDEX_FILE);
+    string line, commitData;
+    bool hasFiles = false;
+    while (getline(indexIn, line)) {
+        commitData += line + "\n";
+        hasFiles = true;
+    }
+    if (!hasFiles) {
+        cout << "Nothing to commit (staging area empty)." << endl;
+        return;
+    }
+
+    // 2. Gather Metadata (The "Node" data)
+    string branch = getCurrentBranch();
+    string parent = getBranchHead(branch);
+    auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    string timestamp = ctime(&now);
+    timestamp.pop_back();
+
+    string metadata = "message: " + message + "\n" +
+                      "parent: " + parent + "\n" +
+                      "branch: " + branch + "\n";
+
+    // 3. Hash the entire commit (Metadata + Files) to get a unique Commit ID
+    string commitHash = Hash(metadata + commitData);
+
+    // 4. Save the commit file
+    ofstream commitOut(COMMITS_DIR + "/" + commitHash);
+    commitOut << metadata << commitData;
+
+    // 5. Update the branch head to point to this new commit
+    updateBranchHead(branch, commitHash);
+
+    // 6. Clear the staging area (Index)
+    ofstream clearIndex(INDEX_FILE, ios::trunc); 
+    
+    cout << "Commit created: " << commitHash << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -75,6 +175,15 @@ int main(int argc, char* argv[]) {
     else if (command == "add" && argc >= 3) {
         add(argv[2]); // argv[2] is the filename you type
     } 
+    else if (command == "commit" && argc >= 4 && string(argv[2]) == "-m") {
+
+    string msg;
+    for (int i = 3; i < argc; i++) {   // start from argv[3]
+        if (i > 3) msg += " ";         // add a space between words
+        msg += argv[i];
+    }
+    commit(msg);
+    }
     else {
         cout << "Unknown command." << endl;
     }
